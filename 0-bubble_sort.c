1121497ee9f857c50b7127d5f2441c44076c09f8
curtarnolds <afayedormawuli@gmail.com>
2023-06-23 04:21:44 +0000

    Big O corrected for bubble sort in 0-O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,9 @@ void bubble_sort(int *array, size_t size)
 	size_t curr, i;
 	int temp = 0, count = 0;
 
+	if (size < 2)
+		return;
+
 	for (i = 0; i < size; i++)
 	{
 		for (curr = 0; curr < size - i - 1; curr++)
