0f902cb9fee00e5fa0febfb745407b8480e8095f
curtarnolds <afayedormawuli@gmail.com>
2023-06-23 03:33:20 +0000

    Bubble sort implemented in 0-bubble_sort.c and time complexity specified in 0-O

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using the
+ * bubble sort algorithm
+ * @array: array of integers to be sorted
+ * @size: size of the array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t curr, i;
+	int temp = 0, count = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (curr = 0; curr < size - i - 1; curr++)
+		{
+			count++;
+			if (array[curr] > array[curr + 1])
+			{
+				printf("count: %d\t", count);
+				temp = array[curr];
+				array[curr] = array[curr + 1];
+				array[curr + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
