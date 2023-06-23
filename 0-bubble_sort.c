71f73d1b347cb5477daa7b891db27e2c51fa50a2
curtarnolds <afayedormawuli@gmail.com>
2023-06-23 04:35:36 +0000

    refactored 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,24 +10,27 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t curr, i;
-	int temp = 0, count = 0;
+	int temp = 0;
+	bool swapped;
 
 	if (size < 2)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
+		swapped = false;
 		for (curr = 0; curr < size - i - 1; curr++)
 		{
-			count++;
 			if (array[curr] > array[curr + 1])
 			{
-				printf("count: %d\t", count);
 				temp = array[curr];
 				array[curr] = array[curr + 1];
 				array[curr + 1] = temp;
 				print_array(array, size);
+				swapped = true;
 			}
 		}
+		if (!swapped)
+			break;
 	}
 }
