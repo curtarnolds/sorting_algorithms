b38ba60fc646dcea34a6f22f02c546871c79affc
curtarnolds <afayedormawuli@gmail.com>
2023-06-28 02:13:45 +0000

    Shell sort implemented in 100-shell_sort.c

---
+++ 100-shell_sort.c
@@ -0,0 +1,59 @@
+#include "sort.h"
+
+
+/**
+ * shell_sort - Sort an array of integers using shell sort with
+ * Knuth's gap sequence (n + 1 = n * 3 + 1)
+ * @array: An array of integers
+ * @size: The size of the array
+*/
+void shell_sort(int *array, size_t size)
+{
+	int *gaps, temp, gapIndex;
+	size_t gap, i, j;
+
+	gaps = (int *) calloc(size, sizeof(int));
+	knuth_gen(gaps, size);
+	gapIndex = 0;
+	while (gaps[gapIndex])
+	{
+		gap = gaps[gapIndex];
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+				array[j] = array[j - gap];
+
+			array[j] = temp;
+		}
+		gapIndex++;
+		print_array(array, size);
+	}
+
+}
+
+
+/**
+ * knuth_gen - Populate an array with Knuth's sequence
+ * @gap_array: The gap array
+ * @size: size of the array
+*/
+void knuth_gen(int *gap_array, size_t size)
+{
+    size_t gap = 1;
+	int j;
+
+    while ((gap * 3) + 1 <= size)
+	{
+        gap = (gap * 3) + 1;
+	}
+
+	j = 0;
+    while (gap > 0)
+	{
+        gap_array[j] = gap;
+        gap = (gap - 1) / 3;
+		j++;
+    }
+}
--- sort.h
+++ sort.h
@@ -24,6 +24,8 @@ void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void quick_sort(int *array, size_t size);
 size_t lomuto_part(int *array, size_t size, int start, int end);
-void swap(int *first, int *second, const int *array, size_t size);
+void swap_arr(int *first, int *second, const int *array, size_t size);
 void sort_algo(int *array, size_t size, int start, int end);
+void shell_sort(int *array, size_t size);
+void knuth_gen(int *gap_array, size_t size);
 #endif
