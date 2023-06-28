7bf361d6943fcbd9cdfe2a66fd3bcf4a2236818a
curtarnolds <afayedormawuli@gmail.com>
2023-06-28 05:04:22 +0000

    Cocktail sort implemented in 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -11,7 +11,7 @@ void cocktail_sort_list(listint_t **list)
 	listint_t *node, *n_node, *p_node;
 	bool swapped = true;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (list == NULL || (*list)->next == NULL)
 		return;
 
 	node = *list;
